5f5fbd1e965281978727a12b4384203fce8a09f0
GildedHonour <gilded.honour@gmail.com>
2013-05-02 07:57:29 +0800

    improvements

--- i_sec_risk_scaper.py
+++ i_sec_risk_scaper.py
@@ -2,31 +2,37 @@ import urllib2
 from BeautifulSoup import BeautifulSoup
 import MySQLdb
 import re
-
+import sys, getopt
 
 base_url = 'http://www.dailyfx.com/calendar'
 request_url_pattern = 'http://www.dailyfx.com/calendar?tz=&sort=date&week={0}%2F{1}&eur=true&usd=true&jpy=true&gbp=true&chf=true&aud=true&cad=true&nzd=true&cny=true&high=true&medium=true&low=true'
 
 def main():
-  html_page = urllib2.urlopen(request_url_pattern.format('2012', '0101'))
-  soup = BeautifulSoup(html_page.read())
-  this_week_link = soup.find('div', {'id': 'e-cal-control-top'}).findAll('span')[1].find('a').get('href', '')
-  next_week_link = None
-  
-  while True:
-    week_data_array = scrape_week_data(soup)
-    next_week_link = soup.find('div', {'id': 'e-cal-control-top'}).findAll('span')[3].find('a').get('href', '')
-    print 'next_week_link ', next_week_link
-    if this_week_link == next_week_link:
-      break
-
-    html_page = urllib2.urlopen(parse_next_week_url(next_week_link))
-    print 'parse_next_week_url(next_week_link)-----', parse_next_week_url(next_week_link)
+  if '--up_till_now' in sys.argv:
+    html_page = urllib2.urlopen(request_url_pattern.format('2012', '0101'))
     soup = BeautifulSoup(html_page.read())
+    this_week_link = soup.find('div', {'id': 'e-cal-control-top'}).findAll('span')[1].find('a').get('href', '')
+    next_week_link = None
+    
+    while True:
+      week_data_array = scrape_week_data_up_till_now(soup)
+      insert_data_up_till_now(week_data_array)
+      
+      next_week_link = soup.find('div', {'id': 'e-cal-control-top'}).findAll('span')[3].find('a').get('href', '')
+      print 'next_week_link ', next_week_link
+      if this_week_link == next_week_link:
+        break
 
-  print 'done!!!!!'
+      html_page = urllib2.urlopen(parse_next_week_url(next_week_link))
+      print 'parse_next_week_url(next_week_link)-----', parse_next_week_url(next_week_link)
+      soup = BeautifulSoup(html_page.read())
 
-def scrape_week_data(soup):
+    current_week_data_array = scrape_current_week_data(soup)
+    upsert_current_week_data(current_week_data_array)
+
+    print 'done!!!!!'
+
+def scrape_week_data_up_till_now(soup):
   last_date = None
   i = 0
   item_list = []
@@ -89,17 +95,31 @@ def parse_next_week_url(next_week_link):
   print ' -----------------------------------'
   return a 
 
-def insert_data_array(data_array):
+def insert_data_up_till_now(data_array):
   db = MySQLdb.connect(host="localhost", user="alex", passwd="", db="test")
   cursor = db.cursor()
-  sql_pattern = 'INSERT INTO T1() VALUES'
+  sql_pattern = 'INSERT INTO ForexCurrencies(Date, Time, Currency, Event, Importance, Actual, Forecast, Previous, Notes) VALUES({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8})'
   for item in data_array:
-    sql = sql_pattern.format(item.fff, item.bbb)
+    sql = sql_pattern.format(item['date'], item['time'], item['currency'], item['event'], item['importance'], item['actual'], item['forecast'], item['previous'], item['notes'])
     cursor.execute(sql)
 
   db.close()
 
+def insert_data_array(data_array):
+  db = MySQLdb.connect(host="localhost", user="alex", passwd="", db="test")
+  cursor = db.cursor()
+  sql_pattern = 'INSERT INTO ForexCurrencies(Date, Time, Currency, Event, Importance, Actual, Forecast, Previous, Notes) VALUES({0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8})'
+  for item in data_array:
+    sql = sql_pattern.format(item['date'], item['time'], item['currency'], item['event'], item['importance'], item['actual'], item['forecast'], item['previous'], item['notes'])
+    cursor.execute(sql)
 
+  db.close()
+
+def scrape_current_week_data(soup):
+  pass
+
+def upsert_current_week_data(data_array):
+  pass
 
 
 if __name__ == "__main__":
